d25fbcddd70415a127e4833883d0d9d1ade70a19
Raven <user@users-MacBook-Air.local>
2022-06-29 22:03:45 -0400

    Beginner Dash

---
+++ app.js
@@ -0,0 +1,93 @@
+const Header = () => {
+  return <h1>Apex Player DashBoard</h1>;
+};
+
+// Player Component
+const Reviews = () => {
+  return (
+    <div className="review">
+     <h2>Player Reviews</h2>
+     <p>Player Review Here</p>
+    </div>
+  );
+};
+
+// Square Component
+const Rating = () => {
+  return (
+    <div>
+      <h4 className="rating">Top Player Ranks</h4>
+      <ul className="top-player">
+        <li>Justin</li>
+        <li>Eron</li>
+        <li>Amad</li>
+        <li>Abe</li>
+        <li>Semiah</li>
+      </ul>
+
+    </div>
+  );
+};
+
+
+const  Analysis = () => {
+  return (
+    <div className="analysis">
+    <h3>Players Experience</h3>
+    <h5>Graph here</h5>
+     
+    </div>
+  );
+};
+
+const Visitors = () => {
+  return (
+    <div className="visit">
+     <h3>Players Online</h3>
+     <p><h4>10.4k</h4></p>
+    </div>
+  );
+};
+
+const PlayMenu = () => {
+  return (
+    <div className="Menu">
+     <h3>Menu</h3>
+     <p><h4>
+      <ul>
+      <li>Community</li>
+      <li>Online Tournements</li>
+      <li>Player Profile</li>
+      <li>Stream</li>
+      <li>Gamer Events</li>
+    </ul>
+
+     </h4></p>
+    </div>
+  );
+};
+
+
+class App extends React.Component {
+    render() {
+        //* only need() for multiple lines
+        //* components are case sensitive 
+        //* Order of components Matters
+        return (
+          <div className='container'>
+       <Header />
+       <Reviews />
+       <Visitors />
+       <Rating />
+      <Analysis />
+      <PlayMenu />
+
+       </div>
+         );
+    }
+}
+
+ReactDOM.render(
+  <App />,
+  document.querySelector('main')
+);
